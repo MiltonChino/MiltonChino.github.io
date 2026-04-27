35363ec5468b88c3aeab83c86c2845958fb23dfc
Milton Chino <miltonchinowrk@gmail.com>
2026-04-27 05:46:56 -0400

    dom basics initial

---
+++ wdd131/ponder/dom.js
@@ -0,0 +1,7 @@
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
+                
