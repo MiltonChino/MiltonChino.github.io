2ae5b7696114166b5471021df205d49ed862724a
Milton Chino <miltonchinowrk@gmail.com>
2026-05-03 09:59:22 -0400

    dom practice complete

--- wdd131/ponder/dom.js
+++ wdd131/ponder/dom.js
@@ -1,7 +1,35 @@
+const title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+let topics = document.querySelector('#topics');
+
+//topics.style.color = 'red';
+
+document.getElementById('topics').style.color = 'blue';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor = 'lightblue';
+
+para.classList.add('background');
+
+//document.querySelector('body').classList.add('background');
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/web.png');
+image.setAttribute('height', '400px');
 
 let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
 })
                 
